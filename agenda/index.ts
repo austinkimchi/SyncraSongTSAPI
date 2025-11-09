f8704601040c7114153672797090735537662bcb
Austin Kim <contact@austin.kim>
2025-11-08 16:09:21 -0800

    add: max concurrency for now

--- agenda/index.ts
+++ agenda/index.ts
@@ -12,7 +12,8 @@ export async function getAgenda(): Promise<Agenda> {
     agenda = await new Agenda({
         db: { collection: 'agendaJobs', address: process.env.MongoURI as string },
         processEvery: '1 second',
-        defaultConcurrency: 5,
+        defaultConcurrency: 1,
+        maxConcurrency: 2,
         defaultLockLifetime: 5 * 60 * 1000,
     });
     registerJobs(agenda);
